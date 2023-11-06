667ad38c6302e09762280f3e41933b63c0504d08
Mael <mael.danet@etu.univ-poitier.fr>
2023-11-06 17:20:50 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react";
-import ListeCharacters from './compenents/ListeCaract';
-import Title from './compenents/title';
-import NumberCharacters from './compenents/NbCharacters';
-const characters = require('./characters.json');
+import './style.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+import routes from './routes';
+
+// Create a router that uses the client side history strategy for
+const router = createBrowserRouter(routes)
 
 function App() {
-return (
-  <>
-    {/* 
-        La propriété id et data-demo sont passées au composant Title grâce au spread operator.
-        La propriété color est définie dans le composant Title
-     */}
-    <Title color="blue" id="my-id" data-demo="demo">Bienvenue sur mon app marvel</Title>
-    <ListeCharacters></ListeCharacters> 
-    <NumberCharacters></NumberCharacters>
 
-
-  </>
-);
+  return (
+    <RouterProvider router={router} />
+  );
 }
 
 export default App;
-
-
